24ccc66429f3161eca23072b9a52fda379399026
saikiran090985254865 <145752384+saikiran090985254865@users.noreply.github.com>
2023-09-22 13:38:54 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+<script>
+
 const userTab = document.querySelector("#data-userWeather");
 const searchTab = document.querySelector("#data-searchWeather");
 const userContainer = document.querySelector(".weather-container");
@@ -10,48 +12,48 @@ const userInfoContainer = document.querySelector(".user-info-container");
 // initially variables need???
 
 let oldTab = userTab;
-const API_KEY = "2cfda335611d1a437e70c74d732f8c00";
+const API_KEY = "d1845658f92b31c64bd94f06f7188c9c";
 oldTab.classList.add("current-tab");
 getfromSessionStorage();
 
 function switchTab(newTab) {
-    if (newTab != oldTab) {
+    if(newTab != oldTab) {
         oldTab.classList.remove("current-tab");
         oldTab = newTab;
         oldTab.classList.add("current-tab");
 
-        if (!searchForm.classList.contains("active")) {
+        if(!searchForm.classList.contains("active")) {
             //kya search form wala container is invisible, if yes then make it visible
             userInfoContainer.classList.remove("active");
             grantAccessContainer.classList.remove("active");
             searchForm.classList.add("active");
         }
         else {
-            //main pehle search wale tab pr tha, ab your weather tab visible karna hai
+            //main pehle search wale tab pr tha, ab your weather tab visible karna h 
             searchForm.classList.remove("active");
             userInfoContainer.classList.remove("active");
-            // ab mai your weather tab me agaya hu, toh weather bhi display karna padega, so let's check local stoage first 
-            // for coordinates, if we haved saved them there.
+            //ab main your weather tab me aagya hu, toh weather bhi display karna poadega, so let's check local storage first
+            //for coordinates, if we haved saved them there.
             getfromSessionStorage();
         }
     }
 }
 
 userTab.addEventListener("click", () => {
-    // pass clicked tab as input parameter 
+    //pass clicked tab as input paramter
     switchTab(userTab);
 });
 
 searchTab.addEventListener("click", () => {
-    // pass clicked tab as input parameter 
+    //pass clicked tab as input paramter
     switchTab(searchTab);
 });
 
-// check if coordinates are already present in session storage
+//check if cordinates are already present in session storage
 function getfromSessionStorage() {
     const localCoordinates = sessionStorage.getItem("user-coordinates");
-    if (!localCoordinates) {
-        //agar local coordinates nahi mila
+    if(!localCoordinates) {
+        //agar local coordinates nahi mile
         grantAccessContainer.classList.add("active");
     }
     else {
@@ -62,26 +64,27 @@ function getfromSessionStorage() {
 }
 
 async function fetchUserWeatherInfo(coordinates) {
-    const { lat, lon } = coordinates;
-    //make grantcontainer invisible
+    const {lat, lon} = coordinates;
+    // make grantcontainer invisible
     grantAccessContainer.classList.remove("active");
-    // make loader visible
+    //make loader visible
     loadingScreen.classList.add("active");
 
-    // API CALL
+    //API CALL
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-
-        const data = await response.json();
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`
+          );
+        const  data = await response.json();
 
         loadingScreen.classList.remove("active");
         userInfoContainer.classList.add("active");
         renderWeatherInfo(data);
-
     }
-    catch (err) {
-        loadingScreen.classList.remove("active")
-        //HomeWork
+    catch(err) {
+        loadingScreen.classList.remove("active");
+        //HW
+
     }
 
 }
@@ -121,11 +124,11 @@ function renderWeatherInfo(weatherInfo) {
 }
 
 function getLocation() {
-    if (navigator.geolocation) {
+    if(navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(showPosition);
     }
     else {
-        //Hw - show an alert for no geolocation available
+        //HW - show an alert for no gelolocation support available
     }
 }
 
@@ -133,7 +136,7 @@ function showPosition(position) {
 
     const userCoordinates = {
         lat: position.coords.latitude,
-        longi: position.coords.longitude,
+        lon: position.coords.longitude,
     }
 
     sessionStorage.setItem("user-coordinates", JSON.stringify(userCoordinates));
@@ -144,18 +147,17 @@ function showPosition(position) {
 const grantAccessButton = document.querySelector("#data-grantAccess");
 grantAccessButton.addEventListener("click", getLocation);
 
-// search bar setup
+const searchInput = document.querySelector("#data-searchInput");
 
-let searchInput = document.querySelector("#data-searchInput");
 searchForm.addEventListener("submit", (e) => {
     e.preventDefault();
     let cityName = searchInput.value;
 
-    if(cityName ==="") 
+    if(cityName === "")
         return;
-    else
-     fetchSearchWeatherInfo(cityName);  
-});
+    else 
+        fetchSearchWeatherInfo(cityName);
+})
 
 async function fetchSearchWeatherInfo(city) {
     loadingScreen.classList.add("active");
@@ -163,15 +165,18 @@ async function fetchSearchWeatherInfo(city) {
     grantAccessContainer.classList.remove("active");
 
     try {
-        const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`);
-
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`
+          );
         const data = await response.json();
         loadingScreen.classList.remove("active");
         userInfoContainer.classList.add("active");
         renderWeatherInfo(data);
     }
     catch(err) {
-        //hw
+        //hW
     }
-
 }
+
+</script>
+
